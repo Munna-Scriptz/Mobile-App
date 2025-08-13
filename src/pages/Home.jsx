6a5563af34a3455a8feb3bdfced48d3b5855d973
Munna-Scripts <munnascriptz@gmail.com>
2025-08-13 17:38:09 +0600

    Features complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import Brands from '../components/Brands'
+import Features from '../components/Features'
 
 const Home = () => {
   return (
     <>
       <Banner/>
       <Brands/>
+      <Features/>
     </>
   )
 }
